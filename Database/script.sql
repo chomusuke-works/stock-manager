b9a804671b21f5d1c2bdbbe97ebf0dd02ad03f04
Chomusuke <yukhanov.artur@gmail.com>
2024-12-14 14:46:36 +0100

    Added database files

---
+++ Database/script.sql
@@ -0,0 +1,90 @@
+CREATE TABLE etagere (
+	id SERIAL PRIMARY KEY,
+	nom VARCHAR(64) NOT NULL,
+	estStock BOOLEAN NOT NULL
+);
+
+CREATE TABLE fournisseur (
+	id SERIAL PRIMARY KEY,
+	nom VARCHAR(64) NOT NULL,
+	email VARCHAR(64) NOT NULL,
+	frequenceCommande INTEGER NOT NULL
+);
+
+CREATE TABLE produit (
+	code INTEGER PRIMARY KEY,
+	nom VARCHAR(64) NOT NULL,
+	prix DECIMAL NOT NULL,
+	idFournisseur INTEGER,
+
+	CONSTRAINT fk_idFournisseur 
+		FOREIGN KEY (idFournisseur) REFERENCES fournisseur(id)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE vente (
+	dateVente DATE,
+	codeProduit INTEGER,
+	vendus INTEGER,
+	jetes INTEGER,
+
+	CONSTRAINT pk_dateVente_codeProduit 
+		PRIMARY KEY (dateVente, codeProduit),
+	CONSTRAINT fk_codeProduit
+		FOREIGN KEY (codeProduit) REFERENCES produit(code)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE lot (
+	dateReception DATE NOT NULL,
+	codeProduit INTEGER NOT NULL,
+	quantite INTEGER NOT NULL, 
+	dateExpiration DATE,
+
+	CONSTRAINT pk_dateReception_codeProduit 
+		PRIMARY KEY (dateReception, codeProduit)
+);
+
+CREATE TABLE segmentAnnee (
+	id SERIAL PRIMARY KEY,
+	nom VARCHAR(64) NOT NULL,
+	dateDebut DATE NOT NULL,
+	dateFin DATE NOT NULL,
+	priorite INTEGER NOT NULL
+);
+
+CREATE TABLE produitSegmentAnnee (
+	codeProduit INTEGER,
+	idSegment INTEGER,
+	cible INTEGER,
+	seuil INTEGER,
+
+	CONSTRAINT pk_codeProduit_idSegment
+		PRIMARY KEY (codeProduit, idSegment),
+	CONSTRAINT fk_codeProduit
+		FOREIGN KEY (codeProduit) REFERENCES produit(code)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	CONSTRAINT fk_idSegment
+		FOREIGN KEY (idSegment) REFERENCES segmentAnnee(id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE produit_etagere (
+	codeProduit INTEGER,
+	idEtagere INTEGER,
+
+	CONSTRAINT pk_codeProduit_idEtagere
+		PRIMARY KEY (codeProduit, idEtagere),
+	CONSTRAINT fk_codeProduit
+		FOREIGN KEY (codeProduit) REFERENCES produit(code)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	CONSTRAINT fk_idEtagere
+		FOREIGN KEY (idEtagere) REFERENCES etagere(id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)
