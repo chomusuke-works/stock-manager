6cb5cef8d73a007e9c021d9b262962362e425a4d
Chomusuke <yukhanov.artur@gmail.com>
2025-01-05 18:46:15 +0100

    Added query to check for products that expire soon

--- Server/src/main/resources/queries/product.sql
+++ Server/src/main/resources/queries/product.sql
@@ -1,3 +1,4 @@
 INSERT INTO produit (code, nom, prix, idfournisseur) VALUES (?, ?, ?, ?);
 SELECT * FROM produit WHERE code = ?;
 DELETE FROM produit WHERE code = ?;
+SELECT code, nom, quantite FROM produit JOIN lot ON produit.code = lot.codeproduit WHERE lot.dateexpiration - current_date <= ?;
