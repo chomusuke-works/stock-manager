62ae0f92eb31b833b99a26df122e32d9e1db26f2
Chomusuke <yukhanov.artur@gmail.com>
2025-01-06 11:33:15 +0100

    Added missing queries

--- Server/src/main/resources/queries/sale_default.sql
+++ Server/src/main/resources/queries/sale_default.sql
@@ -1,4 +1,3 @@
 INSERT INTO vente (datevente, codeproduit, vendus, jetes) VALUES (?, ?, ?, ?);
-SELECT * FROM vente WHERE codeproduit = ? AND datevente = ?;
-DELETE FROM vente WHERE codeproduit = ? AND datevente = ?;
--- A sale is an accounting entry and cannot be erased
+SELECT * FROM vente WHERE datevente = ? AND codeproduit = ?;
+DELETE FROM vente WHERE datevente = ? AND codeproduit = ?;
