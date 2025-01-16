e9ceab95f79885dbfc5e173b68fd426300a565ce
Chomusuke <yukhanov.artur@gmail.com>
2025-01-16 17:05:21 +0100

    Refactoring

--- Server/src/main/java/app/types/ExpiredProductQuantity.java
+++
@@ -1,9 +0,0 @@
-package app.types;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ExpiredProductQuantity(
-	@JsonProperty("code") long code,
-	@JsonProperty("name") String name,
-	@JsonProperty("quantity") int quantity
-) { }
---
+++ Server/src/main/java/app/types/productQuantity.java
@@ -0,0 +1,13 @@
+package app.types;
+
+public class productQuantity {
+	public long code;
+	public String name;
+	public int quantity;
+
+	public productQuantity(long code, String name, int quantity) {
+		this.code = code;
+		this.name = name;
+		this.quantity = quantity;
+	}
+}
