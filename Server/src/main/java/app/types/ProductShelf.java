ee0c1b58ab73fc3a764dc4ee55d04e82cecebcd9
Chomusuke <yukhanov.artur@gmail.com>
2025-01-17 11:47:52 +0100

    Removed redundant class

--- Server/src/main/java/app/types/ProductShelf.java
+++
@@ -1,7 +0,0 @@
-package app.types;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ProductShelf(
-        @JsonProperty("codeproduit") long code,
-        @JsonProperty("idetagere") int id) { }
