8d20d5684a244559ec1bc32f766d5b62febc3acf
Chomusuke <yukhanov.artur@gmail.com>
2025-01-17 18:03:21 +0100

    Capitalized class name

--- Types/src/main/java/ch/stockmanager/types/ProductDateQuantity.java
+++ Types/src/main/java/ch/stockmanager/types/ProductDateQuantity.java
@@ -1,11 +1,11 @@
 package ch.stockmanager.types;
 
-public class productDateQuantity {
+public class ProductDateQuantity {
 	public String name;
 	public String date;
 	public int quantity;
 
-	public productDateQuantity(String name, java.sql.Date date, int quantity) {
+	public ProductDateQuantity(String name, java.sql.Date date, int quantity) {
 		this.name = name;
 		this.date = date.toString();
 		this.quantity = quantity;
