2165bf1b1ff080ffd2aa85749e832df9026953d3
mang0r <benoit.jaouen@heig-vd.ch>
2025-01-18 19:32:06 +0100

    modified productShelf sql request to get all products even if there are in no shelves

--- Server/src/main/resources/queries/shelf_extra.sql
+++ Server/src/main/resources/queries/shelf_extra.sql
@@ -1,2 +1,2 @@
 UPDATE etagere SET nom = ?, eststock = ? WHERE id = ?;
-SELECT produit.nom, sum(lot.quantite), etagere.nom, produit.code, etagere.id FROM produit_etagere JOIN produit ON produit_etagere.codeproduit = produit.code JOIN etagere ON produit_etagere.idetagere = etagere.id JOIN lot ON produit.code = lot.codeproduit GROUP BY produit.nom, etagere.nom, produit.code, etagere.id;
+SELECT produit.nom, sum(lot.quantite), etagere.nom, produit.code, etagere.id FROM produit_etagere RIGHT JOIN produit ON produit_etagere.codeproduit = produit.code LEFT JOIN etagere ON produit_etagere.idetagere = etagere.id LEFT JOIN lot ON produit.code = lot.codeproduit GROUP BY produit.nom, etagere.nom, produit.code, etagere.id;
