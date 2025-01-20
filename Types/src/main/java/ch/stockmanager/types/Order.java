6950dc4c44775e72f414cde783990e87941e61d8
Chomusuke <yukhanov.artur@gmail.com>
2025-01-20 20:13:22 +0100

    Overrode equals and hashCode

--- Types/src/main/java/ch/stockmanager/types/Order.java
+++ Types/src/main/java/ch/stockmanager/types/Order.java
@@ -1,5 +1,7 @@
 package ch.stockmanager.types;
 
+import java.util.Objects;
+
 @SuppressWarnings("unused")
 public class Order {
 	public String name;
@@ -19,5 +21,19 @@ public class Order {
 
 	public int getQuantity() {
 		return quantity;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Order that = (Order) o;
+		return this.quantity == that.quantity && Objects.equals(this.name, that.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, quantity);
 	}
 }
