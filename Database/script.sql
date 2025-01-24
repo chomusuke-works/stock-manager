84c01ef67b7f97ef9031beb0976f96bc386b6e14
Chomusuke <yukhanov.artur@gmail.com>
2025-01-24 17:05:43 +0100

    Created view for product-shelf join

--- Database/script.sql
+++ Database/script.sql
@@ -87,4 +87,23 @@ CREATE TABLE produit_etagere (
 		FOREIGN KEY (idEtagere) REFERENCES etagere(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
-)
+);
+
+
+
+CREATE VIEW produit_etagere_extended AS
+SELECT
+    produit.code AS codeProduit,
+    produit.nom AS nomProduit,
+    etagere.id AS idEtagere,
+    etagere.nom AS nomEtagere,
+    CASE
+        WHEN etagere.eststock IS NULL THEN ''
+        WHEN etagere.eststock THEN 'stock'
+        ELSE
+            'magasin'
+        END AS secteur
+FROM
+    produit
+        LEFT JOIN produit_etagere ON produit.code = produit_etagere.codeproduit
+        LEFT JOIN etagere ON produit_etagere.idetagere = etagere.id;
