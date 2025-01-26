4bf1ab2a26df9c5073439b8afa67796ce23f4776
Chomusuke <yukhanov.artur@gmail.com>
2025-01-26 17:27:36 +0100

    Removed table identifiers

--- Server/src/main/resources/queries/product_extra.sql
+++ Server/src/main/resources/queries/product_extra.sql
@@ -1,9 +1,9 @@
-SELECT nom, lot.dateexpiration, quantite
+SELECT nom, dateexpiration, quantite
 FROM lot JOIN produit ON lot.codeproduit = produit.code
 WHERE lot.dateexpiration IS NOT NULL
     AND lot.dateexpiration - current_date BETWEEN 0 AND ?;
 
-SELECT nom, lot.dateexpiration, quantite
+SELECT nom, dateexpiration, quantite
 FROM lot JOIN produit ON lot.codeproduit = produit.code
 WHERE lot.dateexpiration IS NOT NULL
   AND lot.dateexpiration - current_date < 0;
