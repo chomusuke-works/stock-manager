07c87768a3acf76f4663c317f01a9a061d828e03
Chomusuke <artur.yukhanov@proton.me>
2025-02-24 19:55:54 +0100

    Improved deployment tools, removed windows deployment script

--- dist/first-launch.sh
+++ dist/first-launch.sh
@@ -1,11 +1,13 @@
 source .env
 docker compose up -d database
-sleep 5
+until [ "`docker inspect -f {{.State.Running}} stock-manager-database-1`"=="true" ]; do
+    sleep 0.1;
+done;
 
-docker exec stock-manager_database mkdir -p /dist/
-docker cp ./script.sql stock-manager_database:/dist/script.sql
-docker cp ./sample_content.sql stock-manager_database:/dist/sample_content.sql
-docker exec stock-manager_database psql -f /dist/script.sql
-docker exec stock-manager_database psql -f /dist/sample_content.sql
+docker exec stock-manager-database-1 mkdir -p /dist/
+docker cp ./script.sql stock-manager-database-1:/dist/script.sql
+docker cp ./sample_content.sql stock-manager-database-1:/dist/sample_content.sql
+docker exec stock-manager-database-1 psql $POSTGRES_DB -U $POSTGRES_USER -f /dist/script.sql
+docker exec stock-manager-database-1 psql $POSTGRES_DB -U $POSTGRES_USER -f /dist/sample_content.sql
 
 docker compose up -d
